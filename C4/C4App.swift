713ccf1a30f08c0bd0768feac628f7f2f1e3db76
rubamei1 <rubameshal12@hotmail.com>
2024-12-17 09:28:22 +0300

    Initial Commit

---
+++ C4/C4App.swift
@@ -0,0 +1,17 @@
+//
+//  C4App.swift
+//  C4
+//
+//  Created by Ruba Meshal Alqahtani on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct C4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
