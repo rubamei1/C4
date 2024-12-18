4f076c8cc350aab07a3e1c39ce761274ce0baa33
SumaihAz <sumaih14@gmail.com>
2024-12-18 11:40:07 +0300

    age veiw added

---
+++ C4/Views/AgeView.swift
@@ -0,0 +1,18 @@
+//
+//  AgeView.swift
+//  C4
+//
+//  Created by Sumaih on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+struct AgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgeView()
+}
